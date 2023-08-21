cd15a637ca224d3ecd932dc7af3394782ed871c6
Vasilis Zaritas <126662977+mpilzar@users.noreply.github.com>
2023-08-21 20:53:46 +0300

    Update encrypt_caesarcipher.c

--- encrypt_caesarcipher.c
+++ encrypt_caesarcipher.c
@@ -3,4 +3,5 @@
 
 int main(){
   //Read string, read unsigned int, encrypt string based on int and return the result.
+  //Also make function that prints all possible ciphers to decrypt the string.
 }
